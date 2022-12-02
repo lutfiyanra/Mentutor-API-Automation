b80b32b6a8ef93ec05899027b148c2d4c05ebb2a
lutfiyanra <lutfiyanra@gmail.com>
2022-12-02 15:45:28 +0700

    Success Run Post Login Test

--- src/test/java/starter/Mentutor/AllStepDefs/PostLoginStepDefs.java
+++ src/test/java/starter/Mentutor/AllStepDefs/PostLoginStepDefs.java
@@ -50,4 +50,62 @@ public class PostLoginStepDefs {
         File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginMentee.json");
         mentutorAPI.postLoginUser(json);
     }
+
+    @Given("Post login as admin with invalid email")
+    public void postLoginAsAdminWithInvalidEmail() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginAdminInvalidEmail.json");
+        mentutorAPI.postLoginUser(json);
+    }
+
+    @Then("Should return respons body code {int} not found")
+    public void shouldReturnResponsBodyCodeNotFound(int notFound) {
+        SerenityRest.then().statusCode(notFound);
+    }
+
+    @And("Validate post login user not found json schema")
+    public void validatePostLoginNotFoundJsonSchema() {
+        File json = new File(MentutorAPI.JSON_SCHEMA+"/PostLoginNotFoundJsonSchema.json");
+        SerenityRest.then().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
+
+    @Given("Post login as admin with invalid password")
+    public void postLoginAsAdminWithInvalidPassword() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginAdminInvalidPassword.json");
+        mentutorAPI.postLoginUser(json);
+    }
+
+    @And("Validate post login invalid input from client json schema")
+    public void validatePostLoginInvalidInputFromClientJsonSchema() {
+        File json = new File(MentutorAPI.JSON_SCHEMA+"/PostLoginInvalidInputFromClient.json");
+        SerenityRest.then().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
+
+    @Then("Should return respons body code {int} bad request")
+    public void shouldReturnResponsBodyCodeBadRequest(int badRequest) {
+        SerenityRest.then().statusCode(badRequest);
+    }
+
+    @Given("Post login as admin with invalid all data")
+    public void postLoginAsAdminWithInvalidAllData() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginInvalidAllData.json");
+        mentutorAPI.postLoginUser(json);
+    }
+
+    @Given("Post login as admin with empty email")
+    public void postLoginAsAdminWithEmptyEmail() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginEmptyEmail.json");
+        mentutorAPI.postLoginUser(json);
+    }
+
+    @Given("Post login as admin with empty password")
+    public void postLoginAsAdminWithEmptyPassword() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginEmptyPassword.json");
+        mentutorAPI.postLoginUser(json);
+    }
+
+    @Given("Post login as admin with empty data")
+    public void postLoginAsAdminWithEmptyData() {
+        File json = new File(MentutorAPI.JSON_REQ_BODY+"/PostLoginEmptyData.json");
+        mentutorAPI.postLoginUser(json);
+    }
 }
